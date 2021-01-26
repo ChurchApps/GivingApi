a7a17abda7b9f3211b6da71a096b2f4731a308e8
Jeremy Zongker <jeremy@trilitech.com>
2021-01-26 01:45:33 -0600

    Updated to do nothing instead of throw error if initted

--- src/controllers/ChurchController.ts
+++ src/controllers/ChurchController.ts
@@ -7,28 +7,18 @@ import { GivingBaseController } from "./GivingBaseController";
 @controller("/churches")
 export class ChurchController extends GivingBaseController {
 
-    async validateInit(churchId: number) {
-        const errors = [];
-        const funds = await this.repositories.fund.loadAll(churchId);
-        if (funds.length > 0) errors.push("Church already initialized");
-        return errors;
-    }
-
     @httpPost("/init")
     public async init(req: express.Request<{}, {}, { user: UserInterface, church: ChurchInterface }>, res: express.Response): Promise<any> {
         return this.actionWrapper(req, res, async (au) => {
-            const errors = await this.validateInit(au.churchId);
-            if (errors.length > 0) return this.denyAccess(errors);
-            else {
+            const funds = await this.repositories.fund.loadAll(au.churchId);
+            if (funds.length === 0) {
                 const fund: Fund = { churchId: au.churchId, name: "General Fund" }
                 await this.repositories.fund.save(fund);
-                return {};
-            }
 
+            }
+            return {};
         });
     }
-
-
 }
 
 
