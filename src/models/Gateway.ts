cfe0b76004309542d075a0536e5b758ab69a8623
Jeremy Zongker <jeremy@trilitech.com>
2021-02-20 22:01:38 +0000

    Added table/model/repo/controller for gateways

---
+++ src/models/Gateway.ts
@@ -0,0 +1,7 @@
+export class Gateway {
+    public id?: string;
+    public churchId?: string;
+    public provider?: string;
+    public publicKey?: string;
+    public privateKey?: string;
+}
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,4 +2,5 @@ export { DonationBatchRepository } from "./DonationBatchRepository";
 export { DonationRepository } from "./DonationRepository";
 export { FundDonationRepository } from "./FundDonationRepository";
 export { FundRepository } from "./FundRepository";
+export { GatewayRepository } from "./GatewayRepository";
 export { Repositories } from "./Repositories";
