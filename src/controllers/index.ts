65e00cd7e97157a03458999cbf6b51a0fcdd0585
Jared Martin <jared@livechurchsolutions.org>
2021-04-19 15:15:27 -0500

    Export only the SettingController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,4 @@
-export { CustomBaseController, ErrorController, FormController, FormSubmissionController, NoteController, QuestionController, } from "../apiBase"
+export { SettingController } from "../apiBase"
 export { ChurchController } from "./ChurchController";
 export { DonationBatchController } from "./DonationBatchController";
 export { DonationController } from "./DonationController";
