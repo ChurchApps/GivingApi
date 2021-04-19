6e32eec18d771ba1ce203d8e49bfd8cf04c36925
Jared Martin <jared@livechurchsolutions.org>
2021-04-19 15:26:46 -0500

    Add settings to GivingApi (#13)
    * Add settings to GivingApi
    
    * Export only the SettingController

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
+export { SettingController } from "../apiBase"
 export { ChurchController } from "./ChurchController";
 export { DonationBatchController } from "./DonationBatchController";
 export { DonationController } from "./DonationController";
