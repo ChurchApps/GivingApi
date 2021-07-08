a0714f755fddcd6c2f948c14064c9f4998034393
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-07-08 11:59:16 +0530

    refactor: remove request body type from /churches/init api (#26)
    * remove request body type from /churches/init api
    
    * update submodule

--- src/controllers/ChurchController.ts
+++ src/controllers/ChurchController.ts
@@ -1,6 +1,5 @@
 import { controller, httpPost } from "inversify-express-utils";
 import { Fund } from "../models";
-import { UserInterface, ChurchInterface } from "../helpers";
 import express from "express";
 import { GivingBaseController } from "./GivingBaseController";
 
@@ -8,13 +7,12 @@ import { GivingBaseController } from "./GivingBaseController";
 export class ChurchController extends GivingBaseController {
 
     @httpPost("/init")
-    public async init(req: express.Request<{}, {}, { user: UserInterface, church: ChurchInterface }>, res: express.Response): Promise<any> {
+    public async init(req: express.Request, res: express.Response): Promise<any> {
         return this.actionWrapper(req, res, async (au) => {
             const funds = await this.repositories.fund.loadAll(au.churchId);
             if (funds.length === 0) {
                 const fund: Fund = { churchId: au.churchId, name: "General Fund" }
                 await this.repositories.fund.save(fund);
-
             }
             return {};
         });
