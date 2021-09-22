bc58afffa9c018cd6b31ebdab14780c25bf17b62
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-09-22 10:25:31 +0530

    remove church controller and update submodule (#30)

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,4 @@
 export { SettingController } from "../apiBase"
-export { ChurchController } from "./ChurchController";
 export { DonationBatchController } from "./DonationBatchController";
 export { DonationController } from "./DonationController";
 export { FundController } from "./FundController";
