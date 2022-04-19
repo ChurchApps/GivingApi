7b57d56509941dc5f89c3adc92b388f78be5ab7b
Jeremy Zongker <jeremy@trilitech.com>
2022-04-18 20:49:38 -0500

    Added one-time form

--- src/controllers/FundController.ts
+++ src/controllers/FundController.ts
@@ -7,40 +7,47 @@ import { Permissions } from '../helpers/Permissions'
 @controller("/funds")
 export class FundController extends GivingBaseController {
 
-    @httpGet("/:id")
-    public async get(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.donations.viewSummary)) return this.json({}, 401);
-            else return this.repositories.fund.convertToModel(au.churchId, await this.repositories.fund.load(au.churchId, id));
-        });
-    }
+  @httpGet("/:id")
+  public async get(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.donations.viewSummary)) return this.json({}, 401);
+      else return this.repositories.fund.convertToModel(au.churchId, await this.repositories.fund.load(au.churchId, id));
+    });
+  }
 
-    @httpGet("/")
-    public async getAll(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            return this.repositories.fund.convertAllToModel(au.churchId, await this.repositories.fund.loadAll(au.churchId));
-        });
-    }
+  @httpGet("/churchId/:churchId")
+  public async getForChurch(@requestParam("churchId") churchId: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapperAnon(req, res, async () => {
+      return this.repositories.fund.convertAllToModel(churchId, await this.repositories.fund.loadAll(churchId));
+    });
+  }
 
-    @httpPost("/")
-    public async save(req: express.Request<{}, {}, Fund[]>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.donations.edit)) return this.json({}, 401);
-            else {
-                const promises: Promise<Fund>[] = [];
-                req.body.forEach(fund => { fund.churchId = au.churchId; promises.push(this.repositories.fund.save(fund)); });
-                const result = await Promise.all(promises);
-                return this.repositories.fund.convertAllToModel(au.churchId, result);
-            }
-        });
-    }
+  @httpGet("/")
+  public async getAll(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      return this.repositories.fund.convertAllToModel(au.churchId, await this.repositories.fund.loadAll(au.churchId));
+    });
+  }
 
-    @httpDelete("/:id")
-    public async delete(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.donations.edit)) return this.json({}, 401);
-            else await this.repositories.fund.delete(au.churchId, id);
-        });
-    }
+  @httpPost("/")
+  public async save(req: express.Request<{}, {}, Fund[]>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.donations.edit)) return this.json({}, 401);
+      else {
+        const promises: Promise<Fund>[] = [];
+        req.body.forEach(fund => { fund.churchId = au.churchId; promises.push(this.repositories.fund.save(fund)); });
+        const result = await Promise.all(promises);
+        return this.repositories.fund.convertAllToModel(au.churchId, result);
+      }
+    });
+  }
+
+  @httpDelete("/:id")
+  public async delete(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.donations.edit)) return this.json({}, 401);
+      else await this.repositories.fund.delete(au.churchId, id);
+    });
+  }
 
 }
--- src/controllers/PaymentMethodController.ts
+++ src/controllers/PaymentMethodController.ts
@@ -7,111 +7,114 @@ import { Permissions } from "../helpers/Permissions"
 @controller("/paymentmethods")
 export class PaymentMethodController extends GivingBaseController {
 
-    @httpGet("/personid/:id")
-    public async getPersonPaymentMethods(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            const secretKey = await this.loadPrivateKey(au.churchId);
-            const permission = secretKey && (au.checkAccess(Permissions.donations.view) || id === au.personId);
-            if (!permission) return this.json({}, 401);
-            else {
-                const customer = await this.repositories.customer.loadByPersonId(au.churchId, id);
-                return customer ? await StripeHelper.getCustomerPaymentMethods(secretKey, customer) : [];
-            }
-        });
-    }
+  @httpGet("/personid/:id")
+  public async getPersonPaymentMethods(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      const secretKey = await this.loadPrivateKey(au.churchId);
+      const permission = secretKey && (au.checkAccess(Permissions.donations.view) || id === au.personId);
+      if (!permission) return this.json({}, 401);
+      else {
+        const customer = await this.repositories.customer.loadByPersonId(au.churchId, id);
+        return customer ? await StripeHelper.getCustomerPaymentMethods(secretKey, customer) : [];
+      }
+    });
+  }
 
-    @httpPost("/addcard")
-    public async addCard(req: express.Request<any>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            const secretKey = await this.loadPrivateKey(au.churchId);
-            const { id, personId, customerId, email, name } = req.body;
-            const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || personId === au.personId);
-            if (!permission) return this.json({}, 401);
-            else {
-                let customer = customerId;
-                if (!customer) {
-                    customer = await StripeHelper.createCustomer(secretKey, email, name);
-                    this.repositories.customer.save({ id: customer, churchId: au.churchId, personId });
-                }
-                try { return await StripeHelper.attachPaymentMethod(secretKey, id, {customer}); }
-                catch (e) { return e; }
-            }
-        });
-    }
+  @httpPost("/addcard")
+  public async addCard(req: express.Request<any>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      const secretKey = await this.loadPrivateKey(au.churchId);
+      const { id, personId, customerId, email, name } = req.body;
+      const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || personId === au.personId);
+      if (!permission) return this.json({}, 401);
+      else {
+        let customer = customerId;
+        if (!customer) {
+          customer = await StripeHelper.createCustomer(secretKey, email, name);
+          await this.repositories.customer.save({ id: customer, churchId: au.churchId, personId });
+        }
+        try {
+          const pm = await StripeHelper.attachPaymentMethod(secretKey, id, { customer });
+          return { paymentMathod: pm, customerId: customer }
+        }
+        catch (e) { return e; }
+      }
+    });
+  }
 
-    @httpPost("/updatecard")
-    public async updateCard(req: express.Request<any>, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            const { personId, paymentMethodId, cardData } = req.body;
-            const secretKey = await this.loadPrivateKey(au.churchId);
-            const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || personId === au.personId);
-            if (!permission) return this.json({}, 401);
-            try { return await StripeHelper.updateCard(secretKey, paymentMethodId, cardData); }
-            catch (e) { return e; }
-        });
-    }
+  @httpPost("/updatecard")
+  public async updateCard(req: express.Request<any>, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      const { personId, paymentMethodId, cardData } = req.body;
+      const secretKey = await this.loadPrivateKey(au.churchId);
+      const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || personId === au.personId);
+      if (!permission) return this.json({}, 401);
+      try { return await StripeHelper.updateCard(secretKey, paymentMethodId, cardData); }
+      catch (e) { return e; }
+    });
+  }
 
-    @httpPost("/addbankaccount")
-    public async addBankAccount(req: express.Request<any>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            const { id, personId, customerId, email, name } = req.body;
-            const secretKey = await this.loadPrivateKey(au.churchId);
-            const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || personId === au.personId);
-            if (!permission) return this.json({}, 401);
-            else {
-                let customer = customerId;
-                if (!customer) {
-                    customer = await StripeHelper.createCustomer(secretKey, email, name);
-                    this.repositories.customer.save({ id: customer, churchId: au.churchId, personId });
-                }
-                try { return await StripeHelper.createBankAccount(secretKey, customer, {source: id}) }
-                catch (e) { return e; }
-            }
-        });
-    }
+  @httpPost("/addbankaccount")
+  public async addBankAccount(req: express.Request<any>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      const { id, personId, customerId, email, name } = req.body;
+      const secretKey = await this.loadPrivateKey(au.churchId);
+      const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || personId === au.personId);
+      if (!permission) return this.json({}, 401);
+      else {
+        let customer = customerId;
+        if (!customer) {
+          customer = await StripeHelper.createCustomer(secretKey, email, name);
+          this.repositories.customer.save({ id: customer, churchId: au.churchId, personId });
+        }
+        try { return await StripeHelper.createBankAccount(secretKey, customer, { source: id }) }
+        catch (e) { return e; }
+      }
+    });
+  }
 
-    @httpPost("/updatebank")
-    public async updateBank(req: express.Request<any>, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            const secretKey = await this.loadPrivateKey(au.churchId);
-            const { paymentMethodId, personId, bankData, customerId } = req.body;
-            const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || personId === au.personId);
-            if (!permission) return this.json({}, 401);
-            try { return await StripeHelper.updateBank(secretKey, paymentMethodId, bankData, customerId); }
-            catch (e) { return e; }
-        });
-    }
+  @httpPost("/updatebank")
+  public async updateBank(req: express.Request<any>, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      const secretKey = await this.loadPrivateKey(au.churchId);
+      const { paymentMethodId, personId, bankData, customerId } = req.body;
+      const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || personId === au.personId);
+      if (!permission) return this.json({}, 401);
+      try { return await StripeHelper.updateBank(secretKey, paymentMethodId, bankData, customerId); }
+      catch (e) { return e; }
+    });
+  }
 
-    @httpPost("/verifybank")
-    public async verifyBank(req: express.Request<any>, res: express.Response): Promise<any> {
-        return this.actionWrapper(req, res, async (au) => {
-            const secretKey = await this.loadPrivateKey(au.churchId);
-            const { paymentMethodId, customerId, amountData } = req.body;
-            const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || (await this.repositories.customer.convertToModel(au.churchId, await this.repositories.customer.load(au.churchId, customerId)).personId === au.personId));
-            if (!permission) return this.json({}, 401);
-            else {
-                try { return await StripeHelper.verifyBank(secretKey, paymentMethodId, amountData, customerId); }
-                catch (e) { return e; }
-            }
-        });
-    }
+  @httpPost("/verifybank")
+  public async verifyBank(req: express.Request<any>, res: express.Response): Promise<any> {
+    return this.actionWrapper(req, res, async (au) => {
+      const secretKey = await this.loadPrivateKey(au.churchId);
+      const { paymentMethodId, customerId, amountData } = req.body;
+      const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || (await this.repositories.customer.convertToModel(au.churchId, await this.repositories.customer.load(au.churchId, customerId)).personId === au.personId));
+      if (!permission) return this.json({}, 401);
+      else {
+        try { return await StripeHelper.verifyBank(secretKey, paymentMethodId, amountData, customerId); }
+        catch (e) { return e; }
+      }
+    });
+  }
 
-    @httpDelete("/:id/:customerid")
-    public async delete(@requestParam("id") id: string, @requestParam("customerid") customerId: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            const secretKey = await this.loadPrivateKey(au.churchId);
-            const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || (await this.repositories.customer.convertToModel(au.churchId, await this.repositories.customer.load(au.churchId, customerId)).personId === au.personId));
-            if (!permission) return this.json({}, 401);
-            else {
-                const paymentType = id.substring(0,2);
-                if (paymentType === 'pm') await StripeHelper.detachPaymentMethod(secretKey, id);
-                if (paymentType === 'ba') await StripeHelper.deleteBankAccount(secretKey, customerId, id);
-            }
-        });
-    }
+  @httpDelete("/:id/:customerid")
+  public async delete(@requestParam("id") id: string, @requestParam("customerid") customerId: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      const secretKey = await this.loadPrivateKey(au.churchId);
+      const permission = secretKey && (au.checkAccess(Permissions.donations.edit) || (await this.repositories.customer.convertToModel(au.churchId, await this.repositories.customer.load(au.churchId, customerId)).personId === au.personId));
+      if (!permission) return this.json({}, 401);
+      else {
+        const paymentType = id.substring(0, 2);
+        if (paymentType === 'pm') await StripeHelper.detachPaymentMethod(secretKey, id);
+        if (paymentType === 'ba') await StripeHelper.deleteBankAccount(secretKey, customerId, id);
+      }
+    });
+  }
 
-    private loadPrivateKey = async (churchId: string) => {
-        const gateways = await this.repositories.gateway.loadAll(churchId);
-        return (gateways.length === 0) ? "" : EncryptionHelper.decrypt(gateways[0].privateKey);
-    }
+  private loadPrivateKey = async (churchId: string) => {
+    const gateways = await this.repositories.gateway.loadAll(churchId);
+    return (gateways.length === 0) ? "" : EncryptionHelper.decrypt(gateways[0].privateKey);
+  }
 }
